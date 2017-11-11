378bf582c27cc1e1fe32fd43d2740942e2ac9677
Francis Pilon <francis-pilon@outlook.com>
2017-11-10 20:10:23 -0500

    Refactored functions
    Follow the shoutcast API more, offering more options

--- shoutcast.js
+++ shoutcast.js
@@ -1,85 +1,123 @@
 var Promise = require('promise');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var DOMParser = require('xmldom').DOMParser;
-
+//var DOMParser = require('xmldom').DOMParser;
 var developerID;
-var searchType = {
-    GENRE: {
-        path: 'genresearch',
-        query: 'genre',
-        option: ''
-    },
-    SEARCH: {
-        path: 'stationsearch',
-        query: 'search',
-        option: ''
-    },
-    INFO: {
-        path: 'nowplaying',
-        query: 'ct',
-        option: '&f=xml'
-    },
-    BITRATE: {
-        path: 'advancedsearch',
-        query: 'br',
-        option: '&f=xml'
-    },
-    MEDIA: {
-        path: 'advancedsearch',
-        query: 'mt',
-        option: '&f=xml'
-    },
-    GENREID: {
-        path: 'advancedsearch',
-        query: 'genre_id',
-        option: '&f=xml'
-    }
-}
 
 promise = new Promise(function() {});
 
-var searchStationsXML = function(keyword, type, limit) {
-    return new Promise(function(resolve, reject) {
+// 1.1 Get Top 500 Stations
+var getTop500 = function(option){
+    var limit = option.limit ? "&limit=" + option.limit : "";
+    var br = option.br ? "&br=" + option.br : "";
+    var mt = option.mt ? "&mt=" + option.mt : "";
+
+    var url = "http//api.shoutcast.com/legacy/Top500?k=" + developerID
+    + limit + br + mt;
+
+    return sendRequest(url);
+}
+
+// 1.2 Get Stations by Keyword Search
+var getByKeyword = function(keyword, option){
+    var limit = option.limit ? "&limit=" + option.limit : "";
+    var limitPagination = option.pagination && option.limit ? "," : "";
+    var br = option.br ? "&br=" + option.br : "";
+    var mt = option.mt ? "&mt=" + option.mt : "";
+
+    var url = "http//api.shoutcast.com/legacy/stationsearch?k=" + developerID
+    + limit + pagination + br + mt;
+
+    return sendRequest(url);
+}
+
+// 1.3 Get Stations by Genre
+var getByGenre = function(genre, option){
+    var limit = option.limit ? "&limit=" + option.limit : "";
+    var limitPagination = option.pagination && option.limit ? "," : "";
+    var br = option.br ? "&br=" + option.br : "";
+    var mt = option.mt ? "&mt=" + option.mt : "";
+
+    var url = "http//api.shoutcast.com/legacy/genresearch?k=" + developerID + "&genre=" + genre
+    + limit + pagination + br + mt;
+
+    return sendRequest(url);
+}
+
+// 1.4 Get Stations Based on Now Playing Info
+var getByNowPlaying = function(artist, format, option){
+    var limit = option.limit ? "&limit=" + option.limit : "";
+    var limitPagination = option.pagination && option.limit ? "," : "";
+    var br = option.br ? "&br=" + option.br : "";
+    var mt = option.mt ? "&mt=" + option.mt : "";
+
+    var url = "http//api.shoutcast.com/station/nowplaying?k=" + developerID + "&ct=" + 
+    artist + "&f=" + format + limit + pagination + br + mt;
+
+    return sendRequest(url);
+}
+
+// 1.5 Get Stations by Bitrate or Codec Type or Genre ID
+var getByAdvancedSearch = function(option){
+    var limit = option.limit ? "&limit=" + option.limit : "";
+    var limitPagination = option.pagination && option.limit ? "," : "";
+    var br = option.br ? "&br=" + option.br : "";
+    var mt = option.mt ? "&mt=" + option.mt : "";
+    var genreID = option.genreID ? "&genre_id=" + option.genreID : "";
+    var genre = option.genre ? "&br=" + option.br : "";
+    var format = option.format ? "&f=" + option.format : "";
+
+    var url = "http//api.shoutcast.com/station/advancedsearch?k=" + developerID
+     + limit + format + pagination + br + mt + genreID + genre;
+
+    return sendRequest(url);
+}
+
+// 1.6 Get Random Stations
+var getRandom = function(option){
+    var limit = option.limit ? "&limit=" + option.limit : "";
+    var br = option.br ? "&br=" + option.br : "";
+    var mt = option.mt ? "&mt=" + option.mt : "";
+    var genre = option.genre ? "&br=" + option.br : "";
+    var format = option.format ? "&f=" + option.format : "";
+    
+    var url = "http//api.shoutcast.com/station/stationsearch?k=" + developerID
+    + limit + pagination + br + mt;
+
+    return sendRequest(url);
+}
+
+
+var sendRequest = function(url){
+    return new Promise(resolve, reject){
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                parser = new DOMParser();
-                resolve(parser.parseFromString(this.responseText));
+                resolve(responseText);
+            }
+            else{
+                reject("Could not resolve for " + url + "(Ready State: " + this.readyState + " Status: " + this.status + ")");
             }
         };
-        xhttp.open("GET", "http://api.shoutcast.com/legacy/" + type.path + "?k=" + developerID +
-            "&" + type.query + "=" + keyword + "&limit=" + limit + type.option, true);
-        xhttp.send();
-    });
+    }
+
+    xhttp.open(url, true);
+    xhttp.send();
 }
 
-
-var getSingleStation = function(keyword) {
-    return new Promise(function(resolve, reject) {
-        searchStationsXML(keyword, searchType.SEARCH, 1).then(function(response) {
-            return resolve(response);
-        });
-    });
-}
-
-var getURL = function(keyword) {
-    return new Promise(function(resolve, reject) {
-        getSingleStation(keyword).then(function(response) {
-                var base = response.getElementsByTagName("tunein")[0].getAttribute('base');
-                var stationID = response.getElementsByTagName("station")[0].getAttribute('id');
-                var name = response.getElementsByTagName('station')[0].getAttribute('name');
-                return resolve({stationName: name, url: 'http://yp.shoutcast.com' + base + '?id=' + stationID});
-        });
-    });
+var parseUrl = function(url, base, stationID)
+{
+    return 'http://yp.shoutcast.com' + base + '?id=' + stationID;
 }
 
 module.exports =
-    function(dID) {
-        developerID = dID;
-        return {
-            searchType: searchType,
-            get: Promise.nodeify(getSingleStation),
-            searchStations: Promise.nodeify(searchStationsXML),
-            tuneIn: Promise.nodeify(getURL)
-        };
+function(dID) {
+    developerID = dID;
+    return {
+        getTop500: Promise.nodeify(getTop500),
+        getByKeyword: Promise.nodeify(getByKeyword),
+        getByGenre: Promise.nodeify(getByGenre),
+        getByNowPlaying: Promise.nodeify(getByNowPlaying),
+        getByAdvancedSearch: Promise.nodeify(getByAdvancedSearch),
+        getRandom: Promise.nodeify(getRandom)
     };
+};
